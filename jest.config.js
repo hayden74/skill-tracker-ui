0c28f710530ef9677406d65461d8fd89c184a946
Hayden <hdrd74@gmail.com>
2022-10-01 15:52:23 +1000

    Fixed test bugs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  coverageDirectory: '<rootDir>/target/test-results/',
+  coverageDirectory: '<rootDir>/coverage/',
   testMatch: ['<rootDir>/src/**/@(*.)@(test.js?(x))'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js']
